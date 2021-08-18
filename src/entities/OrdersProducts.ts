e5eb1c0b7ad239151a7778fb6ff82e04e20d0f64
luiizsilverio <luiiz.silverio@gmail.com>
2021-08-18 17:40:25 -0300

    OrdersRepository

---
+++ src/entities/OrdersProducts.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm'
+
+import Order from './Order'
+import Product from './Product'
+
+@Entity('orders_products')
+class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column('decimal')
+  price: number
+
+  @Column('int')
+  quantity: number
+
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({ name: 'order_id'})
+  order: Order
+
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({ name: 'product_id'})
+  product: Product
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
+
+export default OrdersProducts
